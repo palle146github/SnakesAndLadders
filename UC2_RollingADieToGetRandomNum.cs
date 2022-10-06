c65ccf78e38196a6e467481e16491a4a7e105bc3
palle146github <prasanthpalle146@gmail.com>
2022-10-06 15:14:20 +0530

    UC2_RollingADieToGetRandomNum

---
+++ UC2_RollingADieToGetRandomNum.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace SnakesAndLaddersSinglePlayer
+{
+    public class UC2_RollingADieToGetRandomNum
+    {
+        public static void RollingADie()
+        {
+            Random random = new Random();
+            int die1num = random.Next(1, 7);
+            Console.WriteLine(die1num);
+        }
+    }
+}
