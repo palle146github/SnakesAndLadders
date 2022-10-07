9f0215726c3240a12d5f6063144da88b165394db
palle146github <112770521+palle146github@users.noreply.github.com>
2022-10-07 10:26:17 +0530

    Add files via upload

--- UC3_CheckThePosition.cs
+++ UC3_CheckThePosition.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-namespace SnakesAndLaddersSinglePlayer
-{
+
+
     public class UC3_CheckThePosition
     {
         public static void CheckThePosition()
@@ -30,101 +30,105 @@ namespace SnakesAndLaddersSinglePlayer
                     if (position1 > final)
                     {
                         position1 = position1 - die1num;
-                        Console.WriteLine("Position of Player1(" + Player1 + ") is: " + position1);
+
                     }
                     else if (position1 == 4)
                     {
                         position1 = 14;
-                        Console.WriteLine("Position of Player1(" + Player1 + ") is: " + position1);
+
                     }
                     else if (position1 == 9)
                     {
                         position1 = 31;
-                        Console.WriteLine("Position of Player1(" + Player1 + ") is: " + position1);
+
                     }
                     else if (position1 == 20)
                     {
                         position1 = 38;
-                        Console.WriteLine("Position of Player1(" + Player1 + ") is: " + position1);
+
                     }
                     else if (position1 == 28)
                     {
                         position1 = 84;
-                        Console.WriteLine("Position of Player1(" + Player1 + ") is: " + position1);
+
                     }
                     else if (position1 == 40)
                     {
                         position1 = 59;
-                        Console.WriteLine("Position of Player1(" + Player1 + ") is: " + position1);
+
                     }
                     else if (position1 == 51)
                     {
                         position1 = 67;
-                        Console.WriteLine("Position of Player1(" + Player1 + ") is: " + position1);
+
                     }
                     else if (position1 == 63)
                     {
                         position1 = 81;
-                        Console.WriteLine("Position of Player1(" + Player1 + ") is: " + position1);
+
                     }
                     else if (position1 == 71)
                     {
                         position1 = 90;
-                        Console.WriteLine("Position of Player1(" + Player1 + ") is: " + position1);
+
                     }
                     else
                     {
                         position1 = position1;
                     }
-                }       
+                    Console.WriteLine("Position of Player1(" + Player1 + ") is: " + position1);
+
+                }
                 break;
                 case 2:
                 {
                     if (position1 > final)
                     {
                         position1 = position1 - die1num;
-                        Console.WriteLine("Position of Player1(" + Player1 + ") is: " + position1);
+
                     }
                     else if (position1 == 17)
                     {
                         position1 = 7;
-                        Console.WriteLine("Position of Player1(" + Player1 + ") is: " + position1);
+
                     }
                     else if (position1 == 54)
                     {
                         position1 = 34;
-                        Console.WriteLine("Position of Player1(" + Player1 + ") is: " + position1);
+
                     }
                     else if (position1 == 62)
                     {
                         position1 = 19;
-                        Console.WriteLine("Position of Player1(" + Player1 + ") is: " + position1);
+
                     }
                     else if (position1 == 64)
                     {
                         position1 = 60;
-                        Console.WriteLine("Position of Player1(" + Player1 + ") is: " + position1);
+
                     }
                     else if (position1 == 87)
                     {
                         position1 = 24;
-                        Console.WriteLine("Position of Player1(" + Player1 + ") is: " + position1);
+
                     }
                     else if (position1 == 93)
                     {
                         position1 = 73;
-                        Console.WriteLine("Position of Player1(" + Player1 + ") is: " + position1);
+
                     }
                     else if (position1 == 95)
                     {
                         position1 = 75;
-                        Console.WriteLine("Position of Player1(" + Player1 + ") is: " + position1);
+
                     }
                     else if (position1 == 99)
                     {
                         position1 = 78;
-                        Console.WriteLine("Position of Player1(" + Player1 + ") is: " + position1);
+
                     }
+                    Console.WriteLine("Position of Player1(" + Player1 + ") is: " + position1);
+
                 }
                 break;
                 default:
@@ -134,4 +138,3 @@ namespace SnakesAndLaddersSinglePlayer
         Console.ReadKey();
         }
     }
-}
